497d13f24bc0ca6e018ca6d7b27d3ee993f76cd1
Kaique Cardoso <kaiquecardoso@gmail.com>
2026-06-04 13:21:52 -0300

    Update version to 1.0.5 in manifest and package files
    - Bumped the version number in manifest.config.ts, package.json, and package-lock.json to 1.0.5, reflecting the latest changes and improvements.

--- manifest.config.ts
+++ manifest.config.ts
@@ -3,7 +3,7 @@ import { defineManifest } from '@crxjs/vite-plugin'
 export default defineManifest({
   manifest_version: 3,
   name: 'One Tab Manager',
-  version: '1.0.4',
+  version: '1.0.5',
   description:
     'Ao clicar no ícone, a aba atual é fechada e salva na lista. Gerencie tudo na página de opções.',
   permissions: [
